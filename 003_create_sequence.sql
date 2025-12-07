16fe488c5b831b82090aeb0cb1536cf052582f3c
Szabó Dominik <dominik.szabo.03@gmail.com>
2025-12-07 14:15:42 +0100

    DELETE sequence file

--- 003_create_sequence.sql
+++
@@ -1,7 +0,0 @@
-create sequence seq_users start with 1 increment by 1;
-create sequence seq_artists start with 1 increment by 1;
-create sequence seq_albums start with 1 increment by 1;
-create sequence seq_songs start with 1 increment by 1;
-create sequence seq_genres start with 1 increment by 1;
-create sequence seq_playlists start with 1 increment by 1;
-create sequence seq_streams start with 1 increment by 1;
